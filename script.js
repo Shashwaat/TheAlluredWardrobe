2a7a1cc073b313cdd073d1f4a56fa22baedf21de
Shashwat <shashwat.saxena2020@vitbhopal.ac.in>
2023-07-03 11:00:06 +0530

    completed

--- script.js
+++ script.js
@@ -14,3 +14,104 @@ close.addEventListener('click',()=>{
     nav.classList.remove('active');
 })
 }
+
+//carasoul
+const text1_options = [
+    "Person 1",
+    "Person 2",
+    "Person 3",
+    "Person 4"
+  ];
+  const text2_options = [
+    "-Content and Product head",
+    "Logistics Head",
+    "Photographer",
+    "Product Manager"
+  ];
+  const color_options = ["#FFF4E0", "#FCF9BE", "#7FE0EB", "#6CE5B1"];
+  const image_options = [
+    "https://images.unsplash.com/photo-1524721696987-b9527df9e512?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1190&q=80",
+    "https://images.unsplash.com/photo-1556656793-08538906a9f8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+    "https://images.unsplash.com/photo-1506073828772-2f85239b6d2d?ixlib=rb-1.2.1&auto=format&fit=crop&w=1189&q=80",
+    "https://images.unsplash.com/photo-1523800503107-5bc3ba2a6f81?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80"
+  ];
+  var i = 0;
+  const currentOptionText1 = document.getElementById("current-option-text1");
+  const currentOptionText2 = document.getElementById("current-option-text2");
+  const currentOptionImage = document.getElementById("image");
+  const carousel = document.getElementById("carousel-wrapper");
+  const mainMenu = document.getElementById("menu");
+  const optionPrevious = document.getElementById("previous-option");
+  const optionNext = document.getElementById("next-option");
+  
+  currentOptionText1.innerText = text1_options[i];
+  currentOptionText2.innerText = text2_options[i];
+  currentOptionImage.style.backgroundImage = "url(" + image_options[i] + ")";
+  mainMenu.style.background = color_options[i];
+  
+  optionNext.onclick = function () {
+    i = i + 1;
+    i = i % text1_options.length;
+    currentOptionText1.dataset.nextText = text1_options[i];
+  
+    currentOptionText2.dataset.nextText = text2_options[i];
+  
+    mainMenu.style.background = color_options[i];
+    carousel.classList.add("anim-next");
+    
+    setTimeout(() => {
+      currentOptionImage.style.backgroundImage = "url(" + image_options[i] + ")";
+    }, 455);
+    
+    setTimeout(() => {
+      currentOptionText1.innerText = text1_options[i];
+      currentOptionText2.innerText = text2_options[i];
+      carousel.classList.remove("anim-next");
+    }, 650);
+  };
+  
+  optionPrevious.onclick = function () {
+    if (i === 0) {
+      i = text1_options.length;
+    }
+    i = i - 1;
+    currentOptionText1.dataset.previousText = text1_options[i];
+  
+    currentOptionText2.dataset.previousText = text2_options[i];
+  
+    mainMenu.style.background = color_options[i];
+    carousel.classList.add("anim-previous");
+  
+    setTimeout(() => {
+      currentOptionImage.style.backgroundImage = "url(" + image_options[i] + ")";
+    }, 455);
+    
+    setTimeout(() => {
+      currentOptionText1.innerText = text1_options[i];
+      currentOptionText2.innerText = text2_options[i];
+      carousel.classList.remove("anim-previous");
+    }, 650);
+  };
+  
+
+  // Contact us 
+const formHeading = document.querySelector(".form-heading");
+const formInputs = document.querySelectorAll(".contact-form-input");
+
+formInputs.forEach((input) => {
+  input.addEventListener("focus", () => {
+    formHeading.style.opacity = "0";
+    setTimeout(() => {
+      formHeading.textContent = `Your ${input.placeholder}`;
+      formHeading.style.opacity = "1";
+    }, 300);
+  });
+
+  input.addEventListener("blur", () => {
+    formHeading.style.opacity = "0";
+    setTimeout(() => {
+      formHeading.textContent = "Let's Talk";
+      formHeading.style.opacity = "1";
+    }, 300);
+  });
+});
